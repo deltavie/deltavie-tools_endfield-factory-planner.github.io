69bbf7b16d207b504fb9acb5ade1561ed5a510c8
Eric <meaninglesslegacy@gmail.com>
2026-04-02 03:15:58 -0700

    trying to read index from wrong place

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 
-// Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  //output: {
-  //  assetPrefix: '/tools_endfield-factory-planner/',
-  //},
+  output: {
+    assetPrefix: '/tools_endfield-factory-planner/',
+  },
   html: {
     title: 'Endfield Factory Planner'
   },
