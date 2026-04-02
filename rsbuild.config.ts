32f7a5259b05aa8b0c79fb833b1091d440ba2954
Eric <meaninglesslegacy@gmail.com>
2026-04-02 03:04:43 -0700

    asset prefix messing up deployment?

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from '@rsbuild/core';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  output: {
-    assetPrefix: '/tools_endfield-factory-planner/',
-  },
-html: {
+  //output: {
+  //  assetPrefix: '/tools_endfield-factory-planner/',
+  //},
+  html: {
     title: 'Endfield Factory Planner'
   },
 });
